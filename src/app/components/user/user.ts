f94f768e04ad4d2c7e3d7193a338cc8671ef97aa
Young <hogusong1@gmail.com>
2018-09-10 17:03:20 -0700

    first commit

---
+++ src/app/components/user/user.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  // templateUrl: './user.html',
+  // styleUrls: ['./user.css'],
+  template: '<h3>John Smith</h3>',
+  styles: [`h3 { color: orange }`],
+})
+
+export class UserComponent {
+  
+}
